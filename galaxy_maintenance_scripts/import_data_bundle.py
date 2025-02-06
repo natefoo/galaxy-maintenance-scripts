5e2198ef2defbd65365fb6c5c6d5bddaa6e10861
Nate Coraor <nate@bx.psu.edu>
2025-02-06 11:02:33 -0500

    Retry failed requests

--- galaxy_maintenance_scripts/import_data_bundle.py
+++ galaxy_maintenance_scripts/import_data_bundle.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 import click
 import yaml
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 from galaxy.tool_util.data import BundleProcessingOptions
 from galaxy.tools.data import ToolDataTableManager
@@ -14,6 +16,8 @@ CHUNK_SIZE = 65536  # 64k
 
 
 @click.command(help="Import tool data bundle. URI can be a path to a zipped file or directory.")
+@click.option("--retry-backoff", type=float, default=1.0, help="Retry backoff")
+@click.option("--retry-count", type=int, default=5, help="Number of times to retry http failures")
 @click.option("--tool-data-path", type=click.Path(exists=True, resolve_path=True), help="Path were bundle data should be written to")
 @click.option("--data-table-config-path", type=click.Path(exists=True, resolve_path=True), help="Path to tool_data_table_conf.xml file")
 @click.option(
@@ -23,7 +27,8 @@ CHUNK_SIZE = 65536  # 64k
     help="loc file to append data to. Must be be loaded in general data tables",
 )
 @click.argument("uri")
-def run_import_data_bundle(uri: str, tool_data_path: str, data_table_config_path: str, tool_data_file_path: Optional[str] = None):
+def run_import_data_bundle(uri: str, tool_data_path: str, data_table_config_path: str, tool_data_file_path:
+                           Optional[str] = None, retry_count: int, retry_backoff: int):
     table_manager = ToolDataTableManager(
         tool_data_path=tool_data_path,
         config_filename=data_table_config_path,
@@ -42,7 +47,11 @@ def run_import_data_bundle(uri: str, tool_data_path: str, data_table_config_path
             options,
         )
     else:
-        with tempfile.NamedTemporaryFile(mode="wb") as fh, requests.get(uri, stream=True) as r:
+        session = requests.Session()
+        retries = Retry(total=retry_count, backoff_factor=retry_backoff, status_forcelist=[500, 502, 503, 504])
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        with tempfile.NamedTemporaryFile(mode="wb") as fh, session.get(uri, stream=True) as r:
             for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
                 fh.write(chunk)
             fh.flush()
